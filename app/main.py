3220eb7fe5b753cd7c404b2810651d811bbc6a19
Adrian <tesula2013@gmail.com>
2025-05-06 01:47:04 +0300

    Issue3: Create fastAPI application

--- app/main.py
+++ app/main.py
@@ -9,3 +9,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Hello": "LSEG"}
+
+
+@app.post("/log-interpretation/", tags=["Log interpretation"])
+async def log_interpretation():
+    return {"Hello": "Log interpretation"}
+
+
+@app.get("/observability", tags=["Observability"])
+async def observability_report():
+    return {"Hello": "Observability"}
